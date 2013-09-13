4133376f4369fb3713425e0a0d846efaa18e82c3
deadprogram <ron@hybridgroup.com>
2013-09-12 18:13:45 -0700

    Big start on i2c interface support

---
+++ examples/wiichuck.rb
@@ -0,0 +1,17 @@
+require 'artoo'
+
+connection :digispark, :adaptor => :littlewire, :vendor => 0x1781, :product => 0x0c9f
+device :board, :driver => :device_info
+device :wiichuck, :driver => :wiichuck, :connection => :arduino, :interval => 0.1
+
+work do
+  on wiichuck, :c_button => proc { puts "c button pressed!" }
+  on wiichuck, :z_button => proc { puts "z button pressed!" }
+  on wiichuck, :joystick => proc { |*value|
+    puts "joystick x: #{value[1][:x]}, y: #{value[1][:y]}"
+  }
+
+  puts "Firmware name: #{board.firmware_name}"
+  puts "Firmware version: #{board.version}"
+end
+
