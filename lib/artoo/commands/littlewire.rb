3dcc3339896a0dd7cef67565e04defdef3ad69ce
Edgar O Silva <oswaldo.silva@gmail.com>
2014-02-05 19:40:56 -0600

    Minor if refactor.

--- lib/artoo/commands/littlewire.rb
+++ lib/artoo/commands/littlewire.rb
@@ -6,15 +6,12 @@ module Artoo
       package_name "littlewire"
 
       desc "upload", "Uploads littlewire to Digispark devices"
-      option :version, :aliases => "-v", :default => "1.2", :desc => "Version of littlewire protocol to install"
       def upload
         upload_cmd = File.join(File.expand_path(File.dirname(__FILE__)), "deps/littlewireLoader_v13")
 
         case os
         when :linux
-          if copy_udev_rules
-            sleep(5)
-          end
+          sleep(5) if copy_udev_rules
           run(upload_cmd)
         when :macosx
           run(upload_cmd)
